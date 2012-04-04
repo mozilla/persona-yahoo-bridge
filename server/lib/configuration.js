845e4e4b74304cffe2fee28376c204d52a85d7c3
Austin King <shout@ozten.com>
2012-04-04 16:55:40 -0700

    Picking up awsbox config via CONFIG_FILES

--- server/lib/configuration.js
+++ server/lib/configuration.js
@@ -2,10 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var convict = require('convict');
+var convict = require('convict'),
+    fs = require('fs');
 
 var conf = module.exports = convict({
-    browserid_server: 'string = "https://browserid.org"',
+  browserid_server: 'string = "https://browserid.org"',
   client_sessions: {
     cookie_name: 'string = "session_state"',
     secret: 'string = "YOU MUST CHANGE ME"',
@@ -13,3 +14,16 @@ var conf = module.exports = convict({
   },
   issuer: 'string = "dev.bigtent.nutria.org"'
 });
+
+// handle configuration files.  you can specify a CSV list of configuration
+// files to process, which will be overlayed in order, in the CONFIG_FILES
+// environment variable
+if (process.env['CONFIG_FILES']) {
+  var files = process.env['CONFIG_FILES'].split(',');
+  files.forEach(function(file) {
+    console.log('reading ', file);
+    var c = JSON.parse(fs.readFileSync(file, 'utf8'));
+    console.log(c);
+    conf.load(c);
+  });
+}
