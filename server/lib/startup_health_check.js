46743c2a1fe883c5b45191bf41e5ac72ff6a7bd7
Austin King <shout@ozten.com>
2012-07-16 14:35:50 -0700

    Be more paranoid about startup health

--- server/lib/startup_health_check.js
+++ server/lib/startup_health_check.js
@@ -16,6 +16,15 @@ module.exports = function () {
     port: port,
     path: '/__heartbeat__'
   }, function (resp) {
+    var heartbeat = '';
+    resp.on('data', function (data) {
+      heartbeat += data.toString('utf8');
+    });
+    resp.on('end', function (data) {
+      if ('ok certifier' !== heartbeat.trim()) {
+          console.error('Expected a certifier heartbeat response, instead got [' + heartbeat + ']');
+      }
+    });
     if (200 === resp.statusCode) {
       var resp_json = "",
           j = lib.get({
@@ -38,7 +47,6 @@ module.exports = function () {
                     console.log(ours[key]);
                     console.log(theirs[key]);
                     console.error("BigTent and the Certifier have different public keys. This can't end well.");
-                    process.exit(2);
                   }
                 }
               });
@@ -48,6 +56,8 @@ module.exports = function () {
         j.on('error', function (err) {
           console.error('Unable to read public key at ' + host + ':' + port + ' [' + err + ']');
         });
+    } else {
+      console.error('Expected a 200 from /__heartbeat__ on certifier at ' + host + ':' + port + ', but got ' + resp.statusCode);
     }
 
   });
