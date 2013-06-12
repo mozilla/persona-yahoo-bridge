6a68/browserid-bigtent

Alias blacklist to prevent PIN nbrute-forcing, fixes Issue #234

server/lib/event_counter.js
@@ -1,0 +1,129 @@
+/* this Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * An in memory data structure for a fixed capacity counter.
+ * Each counter has an expiration for individual records
+ * such that a user can retry after earlier attempts expire.
+ * Also, records expire, once their individual attempts
+ * have expired, making room for new counters.
+ */
+module.exports = function(options) {
+
+  options.capacity = options.capacity || 10;
+  options.attempts = options.attempts || 5;
+  options.ttl = options.ttlInSeconds || 60 * 10; // 10 Minutes
+
+  // Map email -> [timestamps]
+  var counters = {};
+  var MAX_COUNTERS = options.capacity;
+  var MAX_ATTEMPTS = options.attempts;
+
+  return {
+    /**
+     * Increments a counter based on a key (typically a user's email).
+     * This should be used after checking with EventCounter.allowed()
+     *
+     * @param email - Key for this counter
+     * @throws - Throws an Error if the user has exceeded their max attempts
+     */
+    increment: function(email) {
+      this._ensureCounters(email);
+      if (Object.keys(counters).length > MAX_COUNTERS) {
+        throw new Error(
+          'Programming error, attempted to create more counters that allowed');
+      }
+      if (counters[email].length >= MAX_ATTEMPTS) {
+        throw new Error(
+          'Maximum events reached, unable to increment counter for ' +
+          email + ' ' + counters[email].length);
+      }
+      counters[email].push(new Date().getTime());
+    },
+    /**
+     * Checks an EventCounter instance to see if we can increment a
+     * counter for the given key.
+     * Reasons incrementing may not be allowed:
+     * - Counter for this email have reached max allowed attempts
+     * - Total number of counters in the system have reached max capacity
+     *
+     * @param email - Key for this counter
+     * @return boolean - true if increment is allowed, false otherwise
+     */
+    allowed: function(email) {
+      if (Object.keys(counters).length + 1 >= MAX_COUNTERS) {
+        this._gc();
+      }
+      if (counters[email] && counters[email].length >= MAX_ATTEMPTS) {
+        this._expireCounter(email);
+      }
+      if (Object.keys(counters).length + 1 >= MAX_COUNTERS) return false;
+
+      this._ensureCounters(email);
+      return counters[email].length < MAX_ATTEMPTS;
+    },
+
+    /* private */
+    "_ensureCounters": function(email) {
+      if (!counters[email]) {
+        counters[email] = [];
+      }
+    },
+    "_expireCounter": function(email) {
+      var timestamps = counters[email],
+          expired = [],
+          that = this;
+
+      timestamps.forEach(function(timestamp, i) {
+        var then = new Date(that._getTime());
+        then.setSeconds(then.getSeconds() - options.ttl);
+        if (timestamp < then.getTime()) {
+          expired.push(i);
+        }
+      });
+
+      // Walk backwards to avoid having to sort arrays, etc.
+      for (var i=expired.length - 1; i >= 0; i--) {
+        var index = expired[i];
+        counters[email].splice(index);
+      }
+    },
+    "_gc": function() {
+      var emails = Object.keys(counters),
+          that = this;
+
+      if (! that._getTime) {
+        throw new Error('What missing _getTime');
+      }
+
+      emails.forEach(function(email) {
+        that._expireCounter(email);
+        if (0 === counters[email].length) {
+          delete counters[email];
+        }
+      });
+    },
+    "_getTime": function() {
+      return this.__time || new Date();
+    },
+
+    /* Testing APIs Below */
+    "__time": undefined,
+    "__setTime": function(then) {
+      this.__time = new Date(then);
+    },
+    "__size": function() {
+      return Object.keys(counters).length;
+    },
+    "__dump": function() {
+      Object.keys(counters).forEach(function(email) {
+        console.log('DUMP=', email);
+        for (var i=0; i < counters[email].length; i++) {
+          console.log('DUMP=', email, counters[email][i]);
+        }
+      });
+      return counters;
+    }
+  };
+};

server/lib/pin_code.js
@@ -3,9 +3,16 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const config = require('./configuration'),
+EventCounter = require('./event_counter'),
 logger = require('./logging').logger,
 sekrit = require('./sekrit'),
 session = require('./session_context');
+
+var eventCounter = EventCounter({
+  capacity: config.get('max_pin_codes'),
+  attempts: config.get('max_pin_attempts'),
+  ttl: config.get('pin_counter_ttl_seconds')
+});
 
 /**
  * Validates user session and creates secret PIN
@@ -38,16 +45,20 @@ exports.generateSecret = function(req, cb) {
 exports.validateSecret = function(req, cb) {
   // Issue #218 Take extra time to validate PIN
   setTimeout(function() {
+    var claimedEmail = session.getClaimedEmail(req);
     if (!req.pincodedb) {
       return cb(new Error("Invalid state, missing pin code db cookie"), false);
+    } else if (false === eventCounter.allowed(claimedEmail)) {
+      return cb(new Error("Pin verification disabled"), false);
     }
+    eventCounter.increment(claimedEmail)
     var expectedPin = req.pincodedb.expected_pin;
     if (! req.body || ! req.body.pin) {
       logger.error("Invalid request format");
       return cb(new Error("Invalid request"), false);
     }
     return cb(null, expectedPin === req.body.pin);
-  }, 2000);
+  }, 1000);
 };
 
 /*

mozilla/persona-yahoo-bridge

server/lib/event_counter.js
@@ -1,0 +1,124 @@
+/* this Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * An in memory data structure for a fixed capacity counter.
+ * Each counter has an expiration for individual records
+ * such that a user can retry after earlier attempts expire.
+ * Also, records expire, once their individual attempts
+ * have expired, making room for new counters.
+ */
+module.exports = function(options) {
+
+  options.capacity = options.capacity || 10;
+  options.attempts = options.attempts || 5;
+  options.ttlInSeconds = options.ttlInSeconds || 60 * 10; // 10 Minutes
+
+  // Map email -> [timestamps]
+  var counters = {};
+  var MAX_COUNTERS = options.capacity;
+  var MAX_ATTEMPTS = options.attempts;
+
+  return {
+    /**
+     * Increments a counter based on a key (typically a user's email).
+     * This should be used after checking with EventCounter.allowed()
+     *
+     * @param email - Key for this counter
+     * @throws - Throws an Error if the user has exceeded their max attempts
+     */
+    increment: function(email) {
+      this._ensureCounters(email);
+      if (Object.keys(counters).length > MAX_COUNTERS) {
+        throw new Error(
+          'Programming error, attempted to create more counters that allowed');
+      }
+      if (counters[email].length >= MAX_ATTEMPTS) {
+        throw new Error(
+          'Maximum events reached, unable to increment counter for ' +
+          email + ' ' + counters[email].length);
+      }
+      counters[email].push(new Date().getTime());
+    },
+    /**
+     * Checks an EventCounter instance to see if we can increment a
+     * counter for the given key.
+     * Reasons incrementing may not be allowed:
+     * - Counter for this email have reached max allowed attempts
+     * - Total number of counters in the system have reached max capacity
+     *
+     * @param email - Key for this counter
+     * @return boolean - true if increment is allowed, false otherwise
+     */
+    allowed: function(email) {
+      if (Object.keys(counters).length + 1 >= MAX_COUNTERS) {
+        this._gc();
+      }
+      if (counters[email] && counters[email].length >= MAX_ATTEMPTS) {
+        this._expireCounter(email);
+      }
+      if (Object.keys(counters).length + 1 >= MAX_COUNTERS) return false;
+
+      this._ensureCounters(email);
+      return counters[email].length < MAX_ATTEMPTS;
+    },
+
+    /* private */
+    "_ensureCounters": function(email) {
+      if (!counters[email]) {
+        counters[email] = [];
+      }
+    },
+    "_expireCounter": function(email) {
+      var timestamps = counters[email],
+          expired = [],
+          that = this;
+
+      then = new Date(that._getTime()),
+      then.setSeconds(then.getSeconds() - options.ttlInSeconds);
+
+      // Walk backwards to avoid having to sort arrays, etc.
+      for (var i=expired.length - 1; i >= 0; i--) {
+        var index = expired[i];
+        counters[email].splice(index);
+      }
+    },
+    "_gc": function() {
+      var emails = Object.keys(counters),
+          that = this;
+
+      if (! that._getTime) {
+        throw new Error('What missing _getTime');
+      }
+
+      emails.forEach(function(email) {
+        that._expireCounter(email);
+        if (0 === counters[email].length) {
+          delete counters[email];
+        }
+      });
+    },
+    "_getTime": function() {
+      return this.__time || new Date();
+    },
+
+    /* Testing APIs Below */
+    "__time": undefined,
+    "__setTime": function(then) {
+      this.__time = new Date(then);
+    },
+    "__size": function() {
+      return Object.keys(counters).length;
+    },
+    "__dump": function() {
+      Object.keys(counters).forEach(function(email) {
+        console.log('DUMP=', email);
+        for (var i=0; i < counters[email].length; i++) {
+          console.log('DUMP=', email, counters[email][i]);
+        }
+      });
+      return counters;
+    }
+  };
+};

server/lib/pin_code.js
@@ -3,9 +3,16 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const config = require('./configuration'),
+EventCounter = require('./event_counter'),
 logger = require('./logging').logger,
 sekrit = require('./sekrit'),
 session = require('./session_context');
+
+var eventCounter = EventCounter({
+  capacity: config.get('max_pin_codes'),
+  attempts: config.get('max_pin_attempts'),
+  ttlInSeconds: config.get('pin_counter_ttl_seconds')
+});
 
 /**
  * Validates user session and creates secret PIN
@@ -21,8 +28,6 @@ exports.generateSecret = function(req, cb) {
     if (err) {
       cb(err);
     } else {
-      req.pincodedb = {};
-
       // Remember User's PIN for later verification
       // We have no backend database.
       req.pincodedb.expected_pin = pinCode;
@@ -38,16 +43,20 @@ exports.generateSecret = function(req, cb) {
 exports.validateSecret = function(req, cb) {
   // Issue #218 Take extra time to validate PIN
   setTimeout(function() {
+    var claimedEmail = session.getClaimedEmail(req);
     if (!req.pincodedb) {
       return cb(new Error("Invalid state, missing pin code db cookie"), false);
+    } else if (false === eventCounter.allowed(claimedEmail)) {
+      return cb(new Error("Pin verification disabled"), false);
     }
+    eventCounter.increment(claimedEmail)
     var expectedPin = req.pincodedb.expected_pin;
     if (! req.body || ! req.body.pin) {
       logger.error("Invalid request format");
       return cb(new Error("Invalid request"), false);
     }
     return cb(null, expectedPin === req.body.pin);
-  }, 2000);
+  }, 1000);
 };
 
 /*